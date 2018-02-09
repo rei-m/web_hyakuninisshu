25269aba3f823b706829f264e780bafa7f072f96
rei-m <star.oceans.rm@gmail.com>
2018-02-09 21:33:13 +0900

    add training actions test

--- __tests__/actions/trainings.test.ts
+++ __tests__/actions/trainings.test.ts
@@ -1,7 +1,123 @@
+import { create } from '../factories';
 import { startTraining } from '../../src/actions/trainings';
+import { getStore } from '../../src/appStore';
+import { Karuta } from '../../src/types';
 
 describe('TrainingActionCreator', () => {
-  it('should create startTraining Action', () => {
-    return expect('hoge').toEqual('hoge');
+  describe('startTraining', () => {
+    let karutas: Karuta[];
+
+    beforeEach(() => {
+      karutas = [...Array(100).keys()].map(i =>
+        create<Karuta>('karuta', {
+          color: i < 20 ? 'blue' : 'pink',
+          id: i + 1,
+          kimariji: i % 5 + 1
+        })
+      );
+      getStore().getState().karuta = { karutas };
+    });
+
+    it('should create startTraining Action', () => {
+      const { questions, startedTime } = startTraining(
+        1,
+        100,
+        0,
+        '',
+        0,
+        1
+      ).payload;
+
+      const { correctKaruta, yomifuda, torifudas } = questions[0];
+      expect(questions).toHaveLength(100);
+      expect(yomifuda.firstText).toEqual(correctKaruta.firstKanji);
+      expect(yomifuda.secondText).toEqual(correctKaruta.secondKanji);
+      expect(yomifuda.thirdText).toEqual(correctKaruta.thirdKanji);
+      expect(torifudas.map(f => f.fourthText)).toContain(
+        correctKaruta.fourthKana
+      );
+      expect(torifudas.map(f => f.fifthText)).toContain(
+        correctKaruta.fifthKana
+      );
+      expect(startedTime).not.toBeUndefined();
+    });
+
+    describe('when filter by range', () => {
+      it('should payload has filtered Karuta', () => {
+        const { questions, startedTime } = startTraining(
+          21,
+          40,
+          0,
+          '',
+          0,
+          1
+        ).payload;
+        const karutaIds = questions.map(q => q.correctKaruta.id);
+        expect(questions).toHaveLength(20);
+        expect(karutaIds).toContain(21);
+        expect(karutaIds).toContain(40);
+        expect(startedTime).not.toBeUndefined();
+      });
+    });
+
+    describe('when filter by kimariji', () => {
+      it('should payload has filtered Karuta', () => {
+        const { questions, startedTime } = startTraining(
+          1,
+          100,
+          1,
+          '',
+          0,
+          1
+        ).payload;
+        expect(questions).toHaveLength(20);
+        expect(
+          questions.every(q => q.correctKaruta.kimariji === 1)
+        ).toBeTruthy();
+        expect(startedTime).not.toBeUndefined();
+      });
+    });
+
+    describe('when filter by color', () => {
+      it('should payload has filtered Karuta', () => {
+        const { questions, startedTime } = startTraining(
+          1,
+          100,
+          0,
+          'blue',
+          0,
+          1
+        ).payload;
+        expect(questions).toHaveLength(20);
+        expect(
+          questions.every(q => q.correctKaruta.color === 'blue')
+        ).toBeTruthy();
+        expect(startedTime).not.toBeUndefined();
+      });
+    });
+
+    describe('when switch karuta style', () => {
+      it('should yomiFuda style is kana and toriFuda style is kanji', () => {
+        const { questions, startedTime } = startTraining(
+          1,
+          100,
+          0,
+          '',
+          1,
+          0
+        ).payload;
+
+        const { correctKaruta, yomifuda, torifudas } = questions[0];
+        expect(yomifuda.firstText).toEqual(correctKaruta.firstKana);
+        expect(yomifuda.secondText).toEqual(correctKaruta.secondKana);
+        expect(yomifuda.thirdText).toEqual(correctKaruta.thirdKana);
+        expect(torifudas.map(f => f.fourthText)).toContain(
+          correctKaruta.fourthKanji
+        );
+        expect(torifudas.map(f => f.fifthText)).toContain(
+          correctKaruta.fifthKanji
+        );
+      });
+    });
   });
 });
--- __tests__/factories/index.ts
+++ __tests__/factories/index.ts
@@ -1,3 +1,5 @@
+import { Karuta } from '../../src/types';
+
 const initializerTree: { [key: string]: () => any } = {};
 const idKeyTree: { [key: string]: string } = {};
 const idHolder: { [key: string]: number } = {};
@@ -18,3 +20,31 @@ export const create = <T>(
   }
   throw new Error(`${name}'s factory is not found`);
 };
+
+export const registerFactory = <T>(
+  name: string,
+  initializer: () => T,
+  idKey?: string
+) => {
+  initializerTree[name] = initializer;
+  idKeyTree[name] = idKey ? idKey : name;
+};
+
+registerFactory<Karuta>('karuta', () => ({
+  color: 'blue',
+  creator: '詠み手',
+  fifthKana: 'ごく',
+  fifthKanji: '五句',
+  firstKana: 'しょく',
+  firstKanji: '初句',
+  fourthKana: 'よんく',
+  fourthKanji: '四句',
+  id: 1,
+  imageNo: '001',
+  kimariji: 1,
+  secondKana: 'にく',
+  secondKanji: '二句',
+  thirdKana: 'さんく',
+  thirdKanji: '三句',
+  translation: '歌の訳'
+}));
