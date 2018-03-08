39e4e334422aa95cc32ac4a3361f9b2b51ce5093
rei-m <star.oceans.rm@gmail.com>
2018-03-08 14:27:17 +0900

    react-test-renderer

--- __tests__/components/AboutSection/index.tsx
+++ __tests__/components/AboutSection/index.tsx
@@ -1,43 +1,20 @@
-import { shallow } from 'enzyme';
+import * as ReactTestRenderer from 'react-test-renderer';
 import * as React from 'react';
 import AboutSection from '../../../src/components/AboutSection';
 
-describe('<AboutSection />', () => {
-  it('should render AboutSection component', () => {
-    const wrapper = shallow(<AboutSection />);
-    expect(wrapper.type()).toEqual('a');
-  });
-
-  // it('should not render link when component is active', () => {
-  //   const wrapper = shallow(
-  //     <Link
-  //       active={true}
-  //       filter= {'filter'}
-  //       onClick={jest.fn()}
-  //     >
-  //       test link
-  //     </Link>,
-  //   );
-  //   expect(wrapper.type()).toEqual('span');
-  // });
-
-  // it('simulates click event', () => {
-  //   const mockHandler = jest.fn();
-  //   const wrapper = shallow(
-  //     <Link
-  //       active={false}
-  //       filter= {'filter'}
-  //       onClick={mockHandler}
-  //     >
-  //       test link
-  //     </Link>,
-  //   );
-
-  //   wrapper.simulate('click', {
-  //     preventDefault: () => {
-  //       return;
-  //     },
-  //   });
-  //   expect(mockHandler).toHaveBeenCalled();
-  // });
+it('should render AboutSection component', () => {
+  const renderer = ReactTestRenderer.create(<AboutSection />);
+  expect(renderer.toJSON()).toMatchSnapshot();
 });
+
+// it('simulates click event', () => {
+//   const mockHandler = jest.fn();
+//   const wrapper = shallow(
+//     <Link
+//       active={false}
+//       filter= {'filter'}
+//       onClick={mockHandler}
+//     >
+//       test link
+//     </Link>,
+//   );
