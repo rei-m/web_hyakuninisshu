2157ae768b35ab9765aef6e914708f1827881089
rei-m <star.oceans.rm@gmail.com>
2018-03-13 22:31:08 +0900

    add ExamIndex test

---
+++ __tests__/containers/Exam/index.tsx
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import { shallow, ShallowWrapper } from 'enzyme';
+import ExamIndex from '../../../src/containers/Exam';
+import ExamMenuSection from '../../../src/components/ExamMenuSection';
+import { ROUTE_PATHS } from '../../../src/constants';
+
+describe('<ExamIndex />', () => {
+  let wrapper: ShallowWrapper<{}>;
+  let mockRouter: any;
+
+  beforeEach(() => {
+    mockRouter = {
+      history: {
+        push: jest.fn()
+      },
+      route: {
+        location: { pathname: '/exam' },
+        match: { params: {} }
+      }
+    };
+    wrapper = shallow(<ExamIndex />).dive({
+      context: {
+        router: mockRouter
+      }
+    });
+  });
+
+  it('should render component', () => {
+    expect(wrapper.dive().find(ExamMenuSection).length).toBe(1);
+  });
+
+  it('should push next page to history when components onSubmit fired', () => {
+    wrapper
+      .dive()
+      .find(ExamMenuSection)
+      .props()
+      .onSubmit();
+    expect(mockRouter.history.push).toHaveBeenCalledWith(
+      ROUTE_PATHS.EXAM_QUESTION,
+      { submitTime: expect.any(Number) }
+    );
+  });
+});
