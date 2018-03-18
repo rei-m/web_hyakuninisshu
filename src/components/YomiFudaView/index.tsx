df128ebfce3abc63c978e9e3b0cc1f3ae46bcc22
rei-m <star.oceans.rm@gmail.com>
2018-03-19 01:09:00 +0900

    feat add ripple

--- src/components/YomiFudaView/index.tsx
+++ src/components/YomiFudaView/index.tsx
@@ -16,7 +16,7 @@ const Frame = withAppTheme(styled.div)`
   display: flex;
   align-items: center;
   justify-content: center;
-  background-color: ${({ theme }) => theme.colorThin};
+  background-color: #fff;
   box-shadow: 0 -1px 5px rgba(0, 0, 0, 0.26);
   font-family: 'Sawarabi Mincho';
 `;
