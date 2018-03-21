b182742c6053014633c24695484ce2762c303c78
rei-m <star.oceans.rm@gmail.com>
2018-03-22 01:27:13 +0900

    feat improve ripple

--- src/enhancers/withRipple.tsx
+++ src/enhancers/withRipple.tsx
@@ -1,47 +1,26 @@
 import * as React from 'react';
-import styled, { keyframes, StyledFunction } from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 const rippleAnimation = keyframes`
-  100% {
+  to {
     transform: scale(2);
     opacity: 0;
   }
 `;
 
 const RippleContainer = styled.div`
-  display: inline-block;
   position: relative;
   overflow: hidden;
-`;
 
-interface RippleEffect {
-  readonly key: number;
-  readonly position: {
-    readonly x: number;
-    readonly y: number;
-  };
-  readonly size: number;
-  readonly color: string;
-}
-
-const rippleEffect: StyledFunction<
-  RippleEffect & React.HTMLProps<HTMLDivElement>
-> =
-  styled.div;
-
-const RippleEffect = rippleEffect`
-  position: absolute;
-  left: ${props => `${props.position.x}px`};
-  top: ${props => `${props.position.y}px`};
-  width: ${props => `${props.size}px`};
-  height: ${props => `${props.size}px`};
-  background: ${props => props.color};
-  border-radius: 50%;
-  user-select: none;
-  pointer-events: none;
-  transform: scale(0);
-  opacity: 0.7;
-  animation: ${rippleAnimation} 0.5s linear;
+  & .ripple-effect {
+    position: absolute;
+    border-radius: 50%;
+    user-select: none;
+    pointer-events: none;
+    transform: scale(0);
+    opacity: 0.75;
+    animation: ${rippleAnimation} 1000ms;
+  }
 `;
 
 export interface RippledComponentProps
@@ -49,42 +28,28 @@ export interface RippledComponentProps
   color?: string;
 }
 
-export interface RippledComponentState {
-  ripples: RippleEffect[];
-}
-
 export const withRipple = (WrappedComponent: React.ComponentClass) => {
-  return class RippledComponent extends React.Component<
-    RippledComponentProps,
-    RippledComponentState
-  > {
-    public static defaultProps: RippledComponentProps = { color: '#a9a9a980' };
+  return class RippledComponent extends React.Component<RippledComponentProps> {
+    public static defaultProps: RippledComponentProps = { color: '#fff' };
 
     private container: HTMLDivElement;
-    private idHolder = 0;
+    private clearRippleTimer: number;
 
     constructor(props: RippledComponentProps) {
       super(props);
       this.holdContainer = this.holdContainer.bind(this);
-      this.createRipple = this.createRipple.bind(this);
-      this.removeFinishedRipple = this.removeFinishedRipple.bind(this);
-      this.state = {
-        ripples: []
-      };
+      this.addRipple = this.addRipple.bind(this);
+      this.clearRipple = this.clearRipple.bind(this);
     }
 
     public render() {
-      const { ripples } = this.state;
-
       return (
         <RippleContainer
           innerRef={this.holdContainer}
-          onMouseDown={this.createRipple}
+          onMouseDown={this.addRipple}
+          onMouseUp={this.clearRipple}
         >
           <WrappedComponent {...this.props} />
-          {ripples.map(r => (
-            <RippleEffect {...r} onAnimationEnd={this.removeFinishedRipple} />
-          ))}
         </RippleContainer>
       );
     }
@@ -93,44 +58,39 @@ export const withRipple = (WrappedComponent: React.ComponentClass) => {
       this.container = component;
     }
 
-    private createRipple(event: React.MouseEvent<HTMLDivElement>) {
+    private addRipple(event: React.MouseEvent<HTMLDivElement>) {
       const {
         width,
         height,
         left,
         top
       } = this.container.getBoundingClientRect();
+
+      const effect = document.createElement('span');
+      effect.className = 'ripple-effect';
+      this.container.appendChild(effect);
+
       const size = Math.max(width, height);
-
-      const newRipple = {
-        color: this.props.color as string,
-        key: this.generateRippleId,
-        position: {
-          x: event.clientX - left - size / 2,
-          y: event.clientY - top - size / 2
-        },
-        size
-      };
-
-      this.setState(prevState => {
-        prevState.ripples.push(newRipple);
-        return prevState;
-      });
+      const ripplerStyle = [
+        `left: ${event.clientX - left - size / 2}px`,
+        `top: ${event.clientY - top - size / 2}px`,
+        `width: ${size}px`,
+        `height: ${size}px`,
+        `background-color: ${this.props.color}`
+      ];
+      effect.setAttribute('style', ripplerStyle.join('; '));
     }
 
-    private removeFinishedRipple() {
-      this.setState(prevState => {
-        const afterRipples = [...prevState.ripples];
-        afterRipples.shift();
-        return {
-          ripples: afterRipples
-        };
-      });
-    }
-
-    private get generateRippleId() {
-      this.idHolder += 1;
-      return this.idHolder;
+    private clearRipple() {
+      if (this.clearRippleTimer) {
+        window.clearTimeout(this.clearRippleTimer);
+      }
+      this.clearRippleTimer = window.setTimeout(() => {
+        const ripples = this.container.getElementsByClassName('ripple-effect');
+        [...Array(ripples.length).keys()].reverse().forEach(i => {
+          this.container.removeChild(ripples.item(i));
+        });
+      }, 1000);
     }
   };
 };
