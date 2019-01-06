d73b7b5e48284312771527abf6f6156b16696113
rei-m <star.oceans.rm@gmail.com>
2019-01-06 13:38:22 +0900

    adjust banner style

--- src/components/AdBanner/index.tsx
+++ src/components/AdBanner/index.tsx
@@ -1,14 +1,21 @@
 import * as React from 'react';
 import AdSense from 'react-adsense';
+import styled from '@src/styles/styled-components';
+
+const Container = styled.div`
+  padding: ${({ theme }) => theme.spacing2x};
+`;
 
 const AdBanner: React.FC<{}> = () => (
-  <AdSense.Google
-    client="ca-pub-4104372369598017"
-    slot="9171068817"
-    style={{ display: 'block' }}
-    format="auto"
-    responsive="true"
-  />
+  <Container>
+    <AdSense.Google
+      client="ca-pub-4104372369598017"
+      slot="9171068817"
+      style={{ display: 'block' }}
+      format="auto"
+      responsive="true"
+    />
+  </Container>
 );
 
 export default AdBanner;
