8dd7e5b4e04eaab3166d412b55c5bebd70048579
rei-m <star.oceans.rm@gmail.com>
2019-06-28 13:43:16 +0900

    update dependency

--- src/gatsbyPages/karutas/no.tsx
+++ src/gatsbyPages/karutas/no.tsx
@@ -27,9 +27,7 @@ const KarutasNoPage = ({ pageContext }: Props) => {
   return (
     <SingleContentPageTemplate
       title={`百人一首 - ${karutaNoString} -`}
-      description={`百人一首の${karutaNoString}の歌のページです。作者は${
-        karuta.creator
-      }です。百人一首の暗記を練習できます。百人一首の札の画像や現代語訳も載せています。百人一首の歌の意味に触れながら楽しく覚えましょう。`}
+      description={`百人一首の${karutaNoString}の歌のページです。作者は${karuta.creator}です。百人一首の暗記を練習できます。百人一首の札の画像や現代語訳も載せています。百人一首の歌の意味に触れながら楽しく覚えましょう。`}
       keywords={[`百人一首`, karutaNoString, karuta.creator, `小倉百人一首`, `歌`, `意味`, `歌番号`, `暗記`, `練習`]}
       pageTitle={karutaNoString}
       menuType={MenuType.Material}
