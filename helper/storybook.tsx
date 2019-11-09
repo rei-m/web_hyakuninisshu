ee3d8c5f13ad0974b0031050c7dc1b9f3a5da44b
rei-m <star.oceans.rm@gmail.com>
2019-11-09 22:45:54 +0900

    update dependencies

--- helper/storybook.tsx
+++ helper/storybook.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { RenderFunction } from '@storybook/react';
 import { createStore } from '@src/state';
 
-export const reduxStoreDecorator = (story: RenderFunction, store = createStore()) => (
-  <Provider store={store}>{story()}</Provider>
-);
+export const reduxStoreDecorator = (story: any, store = createStore()) => <Provider store={store}>{story()}</Provider>;
